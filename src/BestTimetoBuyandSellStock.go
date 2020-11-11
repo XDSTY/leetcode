0a33fd3ff9a2f892c3b4678a83533765935548ef
XDSTY <17859735221@163.com>
2020-11-11 21:47:38 +0800

    btree

--- src/BestTimetoBuyandSellStock.go
+++ src/BestTimetoBuyandSellStock.go
@@ -1,6 +1,8 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 /**
 Say you have an array for which the ith element is the price of a given stock on day i.
@@ -34,4 +36,71 @@ func maxProfitSign(prices []int) int {
 		}
 	}
 	return max
+}
+
+/**
+dp 的基本形式
+# 初始化 base case
+dp[0][0][...] = base
+# 进行状态转移
+for 状态1 in 状态1的所有取值：
+    for 状态2 in 状态2的所有取值：
+        for ...
+            dp[状态1][状态2][...] = 求最值(选择1，选择2...)
+对于股票来说：
+状态1: i天数；状态2: k交易次数；状态3: 股票状态0没有持有，1持有
+选择：买入、卖出、不买入或继续持有股票reset
+对于本题来说，状态转移方程为
+dp[i][1][0] = max(dp[i-1][1][0], dp[i-1][1][1] + price[i])
+dp[i][1][1] = max(dp[i-1][1][1], dp[i-1][0][0] - price[i])
+dp[i-1][0][0] 是等于0的，因为只能进行一次交易，所以进行交易之前一直未0
+因为只能进行一次交易，所以k为1，状态转移方程可以进行简化
+dp[i][0] = max(dp[i-1][0], dp[i-1][1] + price[i])
+dp[i][1] = max(dp[i-1][1], -price[i])
+*/
+func maxProfitSignWithDp(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+	dp := make([][]int, len(prices))
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, 2)
+	}
+	for i := 0; i < len(dp); i++ {
+		// 第一天只能进行买入，不能卖出
+		if i == 0 {
+			dp[i][1] = -prices[i]
+			continue
+		}
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
+	}
+	return dp[len(dp)-1][0]
+}
+
+func max(val1, val2 int) int {
+	if val1 < val2 {
+		return val2
+	}
+	return val1
+}
+
+/**
+还可以对上面的代码进行优化，i 只 用到了 i-1的值，可以去掉dp数组，使用两个变量代替
+*/
+func maxProfitSignWithDpOne(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+	dpI0 := 0
+	dpI1 := 0
+	for i := 0; i < len(prices); i++ {
+		if i == 0 {
+			dpI1 = -prices[i]
+			continue
+		}
+		dpI0 = max(dpI0, dpI1+prices[i])
+		dpI1 = max(dpI1, -prices[i])
+	}
+	return dpI0
 }
--- src/BestTimetoBuyandSellStockII.go
+++ src/BestTimetoBuyandSellStockII.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 /**
 Say you have an array prices for which the ith element is the price of a given stock on day i.
 给你一个数组，数组中的，每个元素的下标代表着天数，值代表着这天的股票价格
@@ -26,4 +28,32 @@ func maxProfitII(prices []int) int {
 		}
 	}
 	return total
+}
+
+/**
+0 <= i < len(prices), 0 <= k <= len(prices) / 2
+假设认为买了股票才是一笔交易的完成
+dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k-1][0] - prices[i])
+因为k为正无穷，可以认为k和k-1是一样大的，则上述公式可以为：
+dp[i][k][0] = max(dp[i-1][k][0], dp[i-1][k][1] + prices[i])
+dp[i][k][1] = max(dp[i-1][k][1], dp[i-1][k][0] - prices[i])
+可以进行简化
+dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i])
+i只和i-1有关，则还可以进行简化
+dp0 = max(dp0, dp1 + prices[i])
+dp1 = max(dp1, dp0 - prices[i])
+*/
+func maxProfitIIWithDp(prices []int) int {
+	if len(prices) <= 1 {
+		return 0
+	}
+	dp0, dp1 := 0, math.MinInt32
+	for i := 0; i < len(prices); i++ {
+		tmp := dp0
+		dp0 = max(dp0, dp1+prices[i])
+		dp1 = max(dp1, tmp-prices[i])
+	}
+	return dp0
 }
